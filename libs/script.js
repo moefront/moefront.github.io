0442a0b247b03b7bb2c34047e76cddbbcebd79fa
吟梦 <i@imim.pw>
2015-07-15 21:50:21 +0800

    jQuery Anime Add! Responsive WebPage Reviewed!

--- libs/script.js
+++ libs/script.js
@@ -121,3 +121,11 @@ $(function(){
 	});
 
 });
+
+jQuery(document).ready(function($) {
+	$(".scroll").click(function(event){		
+		event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+});
+		
