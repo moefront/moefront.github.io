de06918abb87d0f255fbc7de407f6fe74dc539e9
吟梦 <i@imim.pw>
2015-07-15 21:11:08 +0800

    Mobile Style&Somewhere fixed Commit

--- libs/mobile.js
+++ libs/mobile.js
@@ -8,8 +8,8 @@ function introduce(){
 	console.log("是时候展现 MoeFront 真正的技术了！");
 	$("#content").css({"display":"none"}).delay(300);
 	$("#content").html("<p id='introduce-text'><span id='introduce-moe'>MOE</span> 是日文中“萌(萌え)”的读音，日语“萌”（萌え）用来形容可爱的女生和男生等。作为二次元的代表，MoeFront的每位成员都带着萌属性。</p>" +
-		"<p id='introduce-text'><span id='introduce-front'>Front</span> 即Front-end，意为Web开发中的前端。我们以学习HTML、CSS 以及JavaScript、AJAX 和 挑战 IFE 暑期任务为目的组成这个团队。</p>" +
-		"<p id='introduce-text'><span id='introduce-ife'>我们为共同的目标，来到相同的地方。<br>IFE，我们来了！</span></p>");
+		"<p id='introduce-text'><span id='introduce-front'>FRONT</span> 即Front-end，意为Web开发中的前端。我们以学习HTML、CSS 以及JavaScript、AJAX 和 挑战 IFE 暑期任务为目的组成这个团队。</p>" +
+		"<p id='introduce-text'><span id='introduce-ife'>我们为共同的目标，来到相同的地方。<br>IFE，我们来了！</span></p><br><br>");
 	$("#content").fadeIn(500);
 }
 
@@ -24,3 +24,40 @@ function showmembers(){
 		'<div class="member"><img src="avatar/sh.jpg" alt="Awoer"><div class="member-introduce">祀画<br>PHP / HTML</div></div><br>');
 	$("#content").fadeIn(500);
 }
+
+function likeus(){
+	console.log("Do you like us?");
+	$("#content").css({"display":"none"}).delay(300);
+	$("#content").html("<p>感谢你对我们的支持w</p><br><br><p><span id='like'><a href='#' onclick='doLike()' id='like'>❤Like Us</a></span></p>");
+	$("#content").fadeIn(500);
+}
+$.ajax({
+	url:"http://front.moe/like.php?cmd=get",
+	dataType:"json",
+	success:function(data){
+		$("#like span").html(data.likes);
+	},
+	error:function(){
+		$("#like span").html("niconiconi~");
+	}
+});
+function doLike(){
+	var $like_num=$("#like span");
+		$.ajax({
+			url:"http://front.moe/like.php?cmd=add",
+			dataType:"json",
+			success:function(data){
+				$("#like span").html(data.likes);
+				$("#thx").fadeIn();
+				$.cookie("moemoeda", "moemoeda"); 
+			},
+			error:function(){
+				$("#like span").html("niconiconi~");
+			}
+		});
+
+
+}
+$("#like").click(function(){
+	doLike();
+})
